59e920f0e531bcd2d8a12058eb10264ea82425fc
dgg32 <dgg321982@gmail.com>
2022-05-09 11:16:03 +0800

    include config

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyphy',
-    version='1.0.3',
+    version='1.0.4',
     license='MIT',
     author="Sixing Huang",
     author_email='dgg321982@gmail.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/dgg32/pyphy',
-    keywords='NCBI taxonomy for python'
-
+    keywords='NCBI taxonomy for python',
+    package_data={'': ['*.config']}
 )
